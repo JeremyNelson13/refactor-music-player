0d8962b4b6726db3666357a88b3f58e518a7c8c8
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-08-31 10:27:29 -0400

    auth

--- controllers/library.js
+++ controllers/library.js
@@ -6,6 +6,8 @@ router.get('/', (req, res) => {
     res.json({ message: 'Hello World!' })
 })
 
+
+
 //register a new user
 router.post('/register', async (req, res) => {
     try {
@@ -15,5 +17,23 @@ router.post('/register', async (req, res) => {
         res.status(500).json(err)
     }
 })
+//login a user
+router.post('/login', async (req, res) => {})
+//get all users
+router.get('/getusers', async (req, res) => {})
+
+
+//get all blogs
+router.get('/getblogs', async (req, res) => {})
+//get a blog by id
+router.get('/getblog/:id', async (req, res) => {})
+//create a new blog
+router.post('/createblog', async (req, res) => {})
+//update a blog
+router.put('/updateblog/:id', async (req, res) => {})
+//delete a blog
+router.delete('/deleteblog/:id', async (req, res) => {})
+
+
 
 module.exports = router;
