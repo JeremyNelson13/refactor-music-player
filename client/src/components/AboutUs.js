feaa7ea99589d973941dbd37f19c393c9459048b
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-09-03 14:19:52 -0400

    commit linking up server + building auth/logins

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -7,7 +7,7 @@ const AboutUs = () => {
         <div>
             <h1 className="text-center">About Us</h1>
             <hr />
-            <p>
+            <p className="text-center">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Donec euismod, nisi vel consectetur interdum, nisl nisi
                 aliquam nisi, euismod euismod nisi nisi euismod nisi.
